e1fb7ac9772dd2b1cf824bde8beae4986b0182f4
ashim <ashhubcorp@gmail.com>
2024-06-04 10:09:14 -0400

    changed main page functionality, changed table visibility on initial loading, change load state so it would prevent the massive amt of sapce it takes, change css aswell so its dynamic and its not clashing. next steps : work on testing.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './MainPage';
 import LocateSpecificsPage from './LocateSpecificsPage';
-import DocsPage from './docs';
+import DataPage from './DataPage';
 import InfoPage from './info';
-
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/locate" element={<LocateSpecificsPage />} />
-        <Route path="/docs" element={<DocsPage />} />
-        <Route path="/info" element={<InfoPage />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/locate" element={<LocateSpecificsPage />} />
+          <Route path="/docs" element={<DataPage />} />
+          <Route path="/info" element={<InfoPage />} />
+        </Routes>
+      </ScrollToTop>
     </Router>
   );
 }
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,45 +1,53 @@
 import React, { useState } from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
 import { useDropzone } from 'react-dropzone';
-import { getDocument } from 'pdfjs-dist';
+import { getDocument, GlobalWorkerOptions } from 'pdfjs-dist';
 import * as pdfjsLib from 'pdfjs-dist/webpack';
 import * as XLSX from 'xlsx';
 import { Link } from 'react-router-dom';
 import './main.css';
 
-// set the worker source for pdf.js
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
+// Set the worker source for pdf.js
+GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
 
 function MainPage() {
   const [files, setFiles] = useState([]);
   const [texts, setTexts] = useState({});
   const [keywords, setKeywords] = useState('');
-  const [results, setResults] = useState([]);
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [selectedPage, setSelectedPage] = useState(null);
+  const [loading, setLoading] = useState(false); // New loading state
 
   const onDrop = (acceptedFiles) => {
     setFiles(acceptedFiles);
+    setLoading(true); // Set loading to true when files are dropped
 
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader();
-      reader.onload = async () => {
-        const typedArray = new Uint8Array(reader.result);
-        const pdf = await getDocument(typedArray).promise;
-        let fullText = '';
-        const textPositions = [];
-        for (let i = 1; i <= pdf.numPages; i++) {
-          const page = await pdf.getPage(i);
-          const textContent = await page.getTextContent();
-          const pageText = textContent.items.map(item => item.str).join(' ');
-          fullText += `Page ${i}: ${pageText}\n\n`;
+    const promises = acceptedFiles.map((file) => {
+      return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onload = async () => {
+          const typedArray = new Uint8Array(reader.result);
+          const loadingTask = getDocument(typedArray);
+          const pdf = await loadingTask.promise;
+          const textPositions = [];
 
-          textPositions.push({ page: i, text: pageText });
-        }
-        setTexts((prevTexts) => ({ ...prevTexts, [file.name]: { fullText, textPositions } }));
-      };
-      reader.readAsArrayBuffer(file);
+          for (let i = 1; i <= pdf.numPages; i++) {
+            const page = await pdf.getPage(i);
+            const textContent = await page.getTextContent();
+            const pageText = textContent.items.map(item => item.str).join(' ');
+
+            textPositions.push({ page: i, text: pageText, items: textContent.items });
+          }
+          setTexts((prevTexts) => ({ ...prevTexts, [file.name]: { textPositions } }));
+          resolve();
+        };
+        reader.readAsArrayBuffer(file);
+      });
     });
+
+    Promise.all(promises).then(() => setLoading(false)); // Set loading to false when all files are processed
+  };
+
+  const findSentences = (text, keyword) => {
+    const sentences = text.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/);
+    return sentences.filter(sentence => sentence.toLowerCase().includes(keyword.toLowerCase()));
   };
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
@@ -48,59 +56,54 @@ function MainPage() {
     const keywordsArray = keywords.split(',').map(kw => kw.trim());
     const results = [];
     Object.keys(texts).forEach((fileName) => {
-      const { fullText, textPositions } = texts[fileName];
+      const { textPositions } = texts[fileName];
       keywordsArray.forEach(keyword => {
-        textPositions.forEach(({ page, text }) => {
-          const regex = new RegExp(`\\b${keyword}\\b`, 'gi');
-          const matches = text.match(regex);
-          if (matches) {
+        textPositions.forEach(({ page, text }, index) => {
+          const sentences = findSentences(text, keyword);
+          sentences.forEach((sentence, i) => {
             results.push({
               keyword,
               fileName: fileName.replace('.pdf', ''),
               page,
-              occurrences: matches.length
+              occurrence: `${i + 1} / ${sentences.length}`,
+              title: sentence
             });
-          }
+          });
         });
       });
     });
     results.sort((a, b) => a.keyword.localeCompare(b.keyword));
-    setResults(results);
+    handleDownload(results); // Trigger download immediately after search
   };
 
-  const handleDownload = () => {
+  const handleDownload = (results) => {
     const worksheet = XLSX.utils.json_to_sheet(results.map(result => ({
       Keyword: result.keyword,
       File: result.fileName,
       Page: `Page ${result.page}`,
-      Occurrences: result.occurrences
+      Occurrence: result.occurrence,
+      Title: result.title
     })));
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Results');
     XLSX.writeFile(workbook, 'pdf_keyword_analyzer.xlsx');
   };
 
-  const handleKeywordClick = (fileName, page) => {
-    setSelectedFile(files.find(file => file.name === `${fileName}.pdf`));
-    setSelectedPage(page);
-  };
-
   return (
-    <div>
+    <div className="data-page">
       <h1>powerFUL searching.</h1>
-      <div {...getRootProps({ className: 'dropzone' })} style={{ border: '2px dashed #ccc', padding: '20px', textAlign: 'center' }}>
+      <div {...getRootProps({ className: 'dropzone' })}>
         <input {...getInputProps()} />
         <p>drag and drop pdf files here, or click to select</p>
       </div>
-      {files.length > 0 && (
-        <div>
-          <h2>uploaded files:</h2>
-          <ul>
-            {files.map((file, index) => (
-              <li key={index}>{file.name}</li>
-            ))}
-          </ul>
-        </div>
+      {loading ? ( // Show loading state
+        <div>Loading...</div>
+      ) : (
+        files.length > 0 && (
+          <div>
+            <h2>Loading complete</h2>
+          </div>
+        )
       )}
       {Object.keys(texts).length > 0 && (
         <div>
@@ -109,45 +112,11 @@ function MainPage() {
           <button onClick={handleSearch}>search</button>
         </div>
       )}
-      {results.length > 0 && (
-        <div>
-          <h2>search results:</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>file</th>
-                <th>keyword</th>
-                <th>page</th>
-                <th>occurrences</th>
-              </tr>
-            </thead>
-            <tbody>
-              {results.map((result, index) => (
-                <tr key={index} onClick={() => handleKeywordClick(result.fileName, result.page)}>
-                  <td>{result.fileName}</td>
-                  <td>{result.keyword}</td>
-                  <td>page {result.page}</td>
-                  <td>{result.occurrences}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <button onClick={handleDownload}>download to excel</button>
-        </div>
-      )}
-      {selectedFile && selectedPage && (
-        <div>
-          <h2>viewing: {selectedFile.name}</h2>
-          <Document file={selectedFile}>
-            <Page pageNumber={selectedPage} />
-          </Document>
-        </div>
-      )}
       <div style={{ marginTop: '20px' }}>
         <Link to="/">Home</Link> |{' '}
         <Link to="/locate">Locate Specifics</Link> |{' '}
         <Link to="/info">Info</Link> |{' '}
-        <Link to="/docs">Website Docs</Link>
+        <Link to="/docs">Enhance Data</Link>
       </div>
     </div>
   );
